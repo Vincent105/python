e88f647d2d0e9156618ab8380519f3a75138942b
Vincent105 <vincent1050917@gmail.com>
2019-05-07 17:56:24 +0800

    delete

--- Python/Case and Alignment.py
+++
@@ -1,6 +0,0 @@
-#Remove . sequences from both ends
-setup = 'a duck goes to a bar...'
-print(setup.strip('.'))
-print(setup.capitalize())
-print(setup.title())
-print(setup.upper())
--- Python/Replace&slice.py
+++
@@ -1,8 +0,0 @@
-name = 'Penny'
-name = name.replace('P','H')
-print(name)
-
-name = 'Henny'
-print('P'+name[1:])
-
-name.replace
