1cd98ede5e71bc9d42187ad066d315d5b6893be7
Vincent105 <vincent1050917@gmail.com>
2019-05-08 11:18:34 +0800

    commit

--- 01_Python_Crash_Course/1001_file_exception/file_reader.py
+++ 01_Python_Crash_Course/1001_file_exception/file_reader.py
@@ -15,3 +15,39 @@ filename = '01_Python_Crash_Course\\1001_file_exception\\pi_digits.txt'
 with open(filename) as file_object:
     for line in file_object:
         print(line.rstrip())
+
+#逐行讀取文檔並且存放於列表
+filename = '01_Python_Crash_Course\\1001_file_exception\\pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())    
+
+#使用文件的內容
+filename =  '01_Python_Crash_Course\\1001_file_exception\\pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
+print(type(pi_string))
+
+#讀取大量數據
+filename_2 =   '01_Python_Crash_Course\\1001_file_exception\\pi_million_digits.txt'
+
+with open(filename_2) as fileobject:
+    lines = fileobject.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string[:52] + '...')    
