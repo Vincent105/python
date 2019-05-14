1771e534ba6eeb6ed15e8ebc952340ca18c18435
vincent105 <vincent1050917@gmail.com>
2019-05-14 22:20:57 +0800

    1080514

--- 04_The_Path_of_Python/07_if/ch7_5.py
+++ 04_The_Path_of_Python/07_if/ch7_5.py
@@ -1,4 +1,4 @@
-players = ['Vincent', 'Esther']
+players = ['Vincent', 'Esther', 'John', 'Frank']
 for player in players:
     print(player.title() + "it's a great game.")
     print("我很期待下一場" + player.title())
---
+++ 04_The_Path_of_Python/07_if/ch7_6.py
@@ -0,0 +1,9 @@
+players = ['Vincent', 'Esther', 'John', 'Frank', 'Jerry']
+print("列印出前三名球員")
+for player in players[:3]:
+    print(player)
+
+print("列印出後三名球員")
+for player in players[-3:]:
+    print(player)
+
