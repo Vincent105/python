6985cafb7c377604906603492ebea1dd8517afbc
vincent105 <vincent1050917@gmail.com>
2019-05-16 00:30:34 +0800

    1080516

--- 05_python-scraping/chapter1/1-basicExample.py
+++ 05_python-scraping/chapter1/1-basicExample.py
@@ -1,9 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+#pip install beautifulsoup4 
+
 html = urlopen("http://pythonscraping.com/pages/page1.html")
 print(html.read())
-
 
 
 
