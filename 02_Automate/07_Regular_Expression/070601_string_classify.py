fb9204412a146af8d97a8347a2a10a838eed60ae
Vincent105 <vincent1050917@gmail.com>
2019-05-15 17:59:08 +0800

    1080515

--- 02_Automate/07_Regular_Expression/070601_string_classify.py
+++ 02_Automate/07_Regular_Expression/070601_string_classify.py
@@ -6,5 +6,38 @@ m = xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 \
 
 print(m)    
 
+#自定義字符分類
 vowelRegex = re.compile(r'[aeiouAEIOU]')
 print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+#範圍表示
+vowelRegex = re.compile(r'[a-eA-E0-9.]')
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+#集合反向匹配!!
+vowelRegex = re.compile(r'[^aeiouAEIOU]')
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+#用插入符號表示必須存在於開頭
+beginWithHello = re.compile(r'^Hello')
+print(beginWithHello.search('Hello World'))
+print(beginWithHello.search('She say Hello.'))
+
+#$表示字串必須以此正則式結束
+endsWithNumber = re.compile(r'\d$')
+print(endsWithNumber.search('Your number is 42'))
+print(endsWithNumber.search('Your number is forty-two.'))
+
+#^\d+$表示字串必須從開始到結束都是數字
+wholeWithNumber = re.compile(r'^\d+$')
+print(wholeWithNumber.search('123145'))
+print(wholeWithNumber.search('123xyz3145'))
+
+
+
+
+
+
+
+
+
