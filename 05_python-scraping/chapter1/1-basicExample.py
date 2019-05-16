017b6090807677fb2a97a6ac54a8a05d275012d9
Vincent105 <vincent1050917@gmail.com>
2019-05-16 17:31:02 +0800

    1080516

--- 05_python-scraping/chapter1/1-basicExample.py
+++ 05_python-scraping/chapter1/1-basicExample.py
@@ -1,10 +1,22 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError
 
-#pip install beautifulsoup4 
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+try:
+    html = urlopen("http://pythonscraping.com/pages/page1.htm")
+except urllib.error.URLError as e:
+    print(e)    
+except urllib.error.HTTPError as g:
+    print(g)        
+else:
+    bsObj = BeautifulSoup(html.read(),"html.parser")
+print(bsObj.html)
+print('\n')
+print(bsObj.html.body)
+print(bsObj.html.body.h1)
+print(bsObj.h1)
+print(bsObj.title)
 
 
 
