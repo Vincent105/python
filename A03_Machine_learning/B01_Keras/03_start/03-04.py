e71dfeec24b852ab23ce1395bf5f2fd3ee3a1400
vincent105 <vincent1050917@gmail.com>
2019-06-24 23:56:33 +0800

    1080624

--- A03_Machine_learning/B01_Keras/03_start/03-04.py
+++ A03_Machine_learning/B01_Keras/03_start/03-04.py
@@ -1,8 +1,31 @@
+# 載入IMDB資料集
 from keras.datasets import imdb
+import numpy as np
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+(train_data, train_labels), (test_data,
+                             test_labels) = imdb.load_data(num_words=10000)
+# print(train_data[0])
+# print(train_labels[0])
+# print(max([max(sequence) for sequence in train_data]))
 
-print(train_data[0])
-print(train_labels[0])
+word_index = imdb.get_word_index()
+reverse_word_index = dict([(value, key)
+                           for (key, value) in word_index.items()])
+decoded_review = ' '.join(reverse_word_index.get(i-3, '?')
+                          for i in train_data[0])
+# print(decoded_review)
 
-print(max([max(sequence) for sequence in train_data]))
+# 將二層的整數List編碼成二元矩陣
+def vectorize_sequence(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequences in enumerate(sequences):
+        results[i, sequences] = 1.
+    return results
+
+x_train = vectorize_sequence(train_data)
+x_test = vectorize_sequence(test_data)
+
+# print(x_train[0])
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(train_labels).astype('float32')
--- A03_Machine_learning/E01_python_for_data_analysis/05_pandas/0501_pd.Series.py
+++ A03_Machine_learning/E01_python_for_data_analysis/05_pandas/0501_pd.Series.py
@@ -1,16 +1,19 @@
 import pandas as pd
+import numpy as np 
 
+# basic pandas
 obj = pd.Series([4, 7, -5, 3])
 print(obj)
 print(obj.index)
 print(obj.values)
 
+# pandas with index
 obj = pd.Series([4, 7, -5, 3],index=['d','c','a','g'])
 print(obj)
 print(obj.index)
 print(obj.values)
 
-
+# with index 
 print(obj['a'])
 
 obj['a'] = 100
@@ -18,5 +21,23 @@ print(obj['a'])
 print(obj[['a','c']])
 
 #運算
+print("運算")
 print(obj[obj > 50])
 print(obj * 2)
+print(np.exp(obj))
+print('d' in obj)
+print('z' in obj)
+
+print("通过字典创建Series")
+sdata = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 'Utah': 5000}
+obj3 = pd.Series(sdata)
+print(obj3)
+
+print("有序排列")
+states = ['California', 'Ohio', 'Oregon', 'Texas']
+obj4 = pd.Series(sdata,index=states)
+print(obj4)
+
+# missing判斷
+print(pd.isnull(obj4))
+print(pd.notnull(obj4))
